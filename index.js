b410ea8c678b30ece3ff7acebd74ec89723ddbd8
Sumit2606 <Sumitpedgulwar5@gmail.com>
2025-01-17 10:53:51 +0000

    c6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();//Respond with "hello god" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('welcome to nodeapp');
+ res.send('welcome to nodeapp1');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
 module.exports = app;
